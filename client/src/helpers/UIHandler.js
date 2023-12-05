c698d89a33ccfc2baa9a0fddfdedfcada4902d5f
awpas50 <awper9794@gmail.com>
2023-12-06 00:53:18 +0800

    Adds drop zone

--- client/src/helpers/UIHandler.js
+++ client/src/helpers/UIHandler.js
@@ -1,10 +1,20 @@
+import ZoneHandler from "./ZoneHandler";
+
 export default class UIHandler {
     constructor(scene) {
+        this.zoneHandler = new ZoneHandler(scene);
+
+        this.buildZones = () => {
+            scene.dropZone = this.zoneHandler.renderZone(200, 400);
+            this.zoneHandler.renderOutline(scene.dropZone);
+        }
+
         this.buildGameText = () => {
             scene.dealCards = scene.add.text(400, 400, "Deal Cards").setFontSize(14).setFontFamily("Trebuchet MS");
         }
 
         this.buildUI = () => {
+            this.buildZones();
             this.buildGameText();
         }
     }
--- client/src/scenes/game.js
+++ client/src/scenes/game.js
@@ -1,3 +1,8 @@
+import CardHandler from "../helpers/CardHandler";
+import DeckHandler from "../helpers/DeckHandler";
+import GameHandler from "../helpers/GameHandler";
+import InteractiveHandler from "../helpers/InteractiveHandler";
+import SocketHandler from "../helpers/SocketHandler";
 import UIHandler from "../helpers/UIHandler";
 
 export default class Game extends Phaser.Scene {
@@ -9,13 +14,25 @@ export default class Game extends Phaser.Scene {
 
     // before creation
     preload() {
-
+        this.load.image('I001', require('../assets/ICard/I001.png').default);
+        this.load.image('I002', require('../assets/ICard/I002.png').default);
+        this.load.image('I003', require('../assets/ICard/I003.png').default);
+        this.load.image('I004', require('../assets/ICard/I004.png').default);
+        this.load.image('I005', require('../assets/ICard/I005.png').default);
+        this.load.image('I006', require('../assets/ICard/I006.png').default);
+        this.load.image('I007', require('../assets/ICard/I007.png').default);
     }
 
     // just like void Start() in Unity
     create() {
+        this.CardHandler = new CardHandler();
+        this.DeckHandler = new DeckHandler(this);
+        this.GameHandler = new GameHandler(this);
+        this.SocketHandler = new SocketHandler(this);
         this.UIHandler = new UIHandler(this);
         this.UIHandler.buildUI();
+
+        this.InteractiveHandler = new InteractiveHandler(this);
     }
 
     update() {
