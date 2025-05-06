6dc0a0e5ef114e44d4c44c75b52f5a9c77ce400f
awpas50 <awper9794@gmail.com>
2025-05-07 00:05:42 +0800

    change domain from heroku to render

--- client/src/helpers/SocketHandler.js
+++ client/src/helpers/SocketHandler.js
@@ -9,7 +9,7 @@ export default class SocketHandler {
     constructor(scene) {
         // Heroku URL
         // Default: localhost:3000 is where the server is.
-        scene.socket = io("https://ultimate-card-game-f26046605e38.herokuapp.com")
+        scene.socket = io("https://ultimatecardgame.onrender.com")
         // scene.socket = io("http://localhost:3000/")
 
         //Create or join a room
